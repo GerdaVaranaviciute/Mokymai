581d28c577c5ffafae69db811c2dcc8a09c4c0ef
gerda.va <gerda.va@LT235-PC663>
2023-10-16 00:08:07 +0300

    Add project files.

---
+++ Namu_darbas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namu_darbas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "Gerda";
+            string pavarde = "Varanaviciute";
+            int amzius = 28;
+            float ugis = 1.69f;
+            float svoris = 80.1f;
+            string aukstojiMokykla = "Vilniaus Universitetas";
+            int akademinesGrupesKodas = 9958674;
+            int kursas = 1;
+            string studijuProgramosPavadinimas = "Ekonomika";
+            int atsiskaitytuKredituSkaicius = 402;
+
+        }
+    }
+}
